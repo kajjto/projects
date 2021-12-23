5e56ff9b1f3f0c0a819edfdbaa20531ab81c432f
kajjto <96242049+kajjto@users.noreply.github.com>
2021-12-23 17:12:54 +0300

    Add files via upload

---
+++ modul1/modul1/module1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	string fileName;
+	ifstream fin;
+	char c;
+
+	while (true) {
+		cout << "¬ведите название текстового файла\n(путь к нему, если он не находитс€ в той же директории что и прога)" << endl;
+		cin >> fileName;
+
+		fin.open(fileName, ios::in);
+		if (!fin.is_open()) {
+			cout << "‘айл не найден" << endl;
+		}
+		else {
+			while (!fin.eof()) {
+				c = fin.get();
+				if (c != '€')
+					cout << c;
+			}
+			cout << endl << endl;
+			fin.close();
+		}
+	}
+}
